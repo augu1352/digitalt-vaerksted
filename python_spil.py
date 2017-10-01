00843507c79abee97d9e88b243a11f8d3d2be09c
augu1352 <32144571+augu1352@users.noreply.github.com>
2017-10-01 19:48:54 +0200

    01/10-2017 2

--- python_spil.py
+++ python_spil.py
@@ -1,6 +1,50 @@
 import random
 import time
-alfabet = "abcdefghijklmnopqrstuvwxyz"
+
+
+def Saenkeslagskibe():
+    board = []
+    for x in range(5):
+        board.append(["O"] * 5)
+
+    def print_board(board):
+        for row in board:
+            print(" ".join(row))
+
+    print("Lad os spille Saenkeslagskibe!")
+    print_board(board)
+
+    def random_row(board):
+        return random.randint(0, len(board) - 1)
+
+    def random_col(board):
+        return random.randint(0, len(board[0]) - 1)
+
+    ship_col = random_col(board)
+    ship_row = random_row(board)
+    print(ship_col, ship_row)
+
+    for turn in range(5):
+        guess_col = int(input("Gaet Horisontal: "))
+        guess_row = int(input("Gaet Vertikal: "))
+
+        if guess_row == ship_row and guess_col == ship_col:
+            print("\nTillykke! du har sunket mit slagskib!")
+            print("ture: ", turn + 1)
+            break
+        else:
+            if (guess_row < 0 or guess_row > 5) or (guess_col < 0 or guess_col > 5):
+                print("\nOops, det var ikke engang i havet.")
+            elif(board[guess_row][guess_col] == "X"):
+                print("\nDen har du allerede gaettet før.")
+            else:
+                print("\nDu ramte ikke mit slagskib!")
+                board[guess_row][guess_col] = "X"
+        if turn == 4:
+            time.sleep(1.5)
+            print("GAME OVER!\n")
+        print("ture: ", turn + 1, "\n")
+        print_board(board)
 
 
 def RandomNumber():
@@ -28,6 +72,7 @@ def RandomNumber():
 
 
 def RandomLetter():
+    alfabet = "abcdefghijklmnopqrstuvwxyz"
     print("Gaet et bogstav mellem A og Z.")
     random_letter = random.choice(alfabet)
     fundet = False
@@ -53,8 +98,8 @@ def RandomLetter():
 
 
 def menu():
-    spil = ["[TAL-JAGT = 1]", "[BOGSTAV-JAGT = 2]"]
-    print("=" * 80)
+    spil = ["[TAL-JAGT = 1]", "[BOGSTAV-JAGT = 2]", "[Saenkeslagskibe = 3]"]
+    print("=" * 120)
     print("Alle spil:")
     print(" ".join(spil))
     valg = str(input("Vaelg et spil: "))
@@ -63,6 +108,8 @@ def menu():
         RandomNumber()
     elif valg == "2":
         RandomLetter()
+    elif valg == "3":
+        Saenkeslagskibe()
     else:
         menu()
 
@@ -70,16 +117,10 @@ def menu():
 quit = True
 
 
-def TryAgain(n):
-    n = input("Vil du spille et nyt spil? j/n ")
-    if n == "n":
-        n = False
-    elif n == "j":
-        n = True
-    else:
-        TryAgain()
-
-
 while quit is True:
     menu()
-    TryAgain(quit)
+    user_quit = input("\nVil du spille et nyt spil? j/n ")
+    if user_quit == "n":
+        quit = False
+    elif user_quit == "j":
+        quit = True
