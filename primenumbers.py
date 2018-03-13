12ac5edadac1034e5ac240b53fa892c472055734
augu1352 <32144571+augu1352@users.noreply.github.com>
2018-03-13 17:26:03 +0100

    done!

--- primenumbers.py
+++ primenumbers.py
@@ -1,6 +1,21 @@
 class Generator:
-    def __init__(self, amount):
-        numbers = []
-        for n in range(int(amount) + 1):
+    def __init__(self):
+        pass
+    def findPrimes(self, amount):
+        Pnumbers = []
+        for n in range(1, int(amount) + 1):
+            isPrime = True
             for i in range(2, n):
-                if not n % i == 0
+                if i == n or i == 1:
+                    break
+                if n % i == 0:
+                    isPrime = False
+                    break
+            if isPrime:
+                Pnumbers.append(n)
+
+        return Pnumbers
+
+g = Generator()
+print(g.findPrimes(1000000))
+
